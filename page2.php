06e7855c701f17c3d19caf917acb27c7dbf25468
Hina Lilaram <hinalilaram@gmail.com>
2021-10-30 18:44:26 +0500

    Added null check

--- page2.php
+++ page2.php
@@ -6,11 +6,11 @@
 <body>
 <div>
 <h2> YOUR DATA:</h2>
-<p><label for="name">Name: </label> <?php echo $_POST["sname"]?></P>
-<p><label for="Father-name">Father Name: </label> <?php echo $_POST["fname"]?></p>
-<p><label for="Cnic">Cnic Number: </label><?php echo $_POST["nic"]?></p>
-<p><label for="Email">Email Address: </label><?php echo $_POST["mail"]?></p>
-<p><label for="Phone">Contact number: </label><?php echo $_POST["phoneNo"]?></p>
+<p><label for="name">Name: </label> <?php echo isset($_POST['sname']) ? $_POST['sname'] : ''; ?></P>
+<p><label for="Father-name">Father Name: </label> <?php echo isset($_POST['fname']) ? $_POST['fname'] : ''; ?></p>
+<p><label for="Cnic">Cnic Number: </label><?php echo isset($_POST['nic']) ? $_POST['nic'] : ''; ?></p>
+<p><label for="Email">Email Address: </label><?php echo isset($_POST['mail']) ? $_POST['mail'] : ''; ?></p>
+<p><label for="Phone">Contact number: </label><?php echo isset($_POST['phoneno']) ? $_POST['phoneno'] : ''; ?></p>
 </div>
 <div>
 <a href="labTen.php"><button type="submit" name="modify">Modify</button></a>
